f5bb7a8f292d546ca6b117812de48f97d123c9b8
NS <Nigus.Solomon.Dev@outlook.com>
2026-07-13 23:03:48 +0300

    Add Task schema with Mongoose

---
+++ src/tasks/schemas/task.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TaskDocument = Task & Document;
+
+@Schema()
+export class Task {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ default: false })
+  isCompleted: boolean;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
