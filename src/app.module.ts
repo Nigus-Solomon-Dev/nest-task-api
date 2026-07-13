b8dd6c929448cb29bd25b6f5c7e38df3a1671e1a
NS <Nigus.Solomon.Dev@outlook.com>
2026-07-13 23:03:35 +0300

    Initialize NestJS project with basic structure

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskModule } from './tasks/task.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/taskdb'),
+    TaskModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
