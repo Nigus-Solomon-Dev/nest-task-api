c5e87a22c823e66b63f35c5819d218ce70b00469
NS <Nigus.Solomon.Dev@outlook.com>
2026-07-14 22:26:57 +0300

    feat: add class-validator decorators and enable global ValidationPipe

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,17 @@
+import { IsString, IsNotEmpty, IsBoolean, IsOptional } from 'class-validator';
+
 export class UpdateTaskDto {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   title?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   description?: string;
+
+  @IsOptional()
+  @IsBoolean()
   isCompleted?: boolean;
 }
